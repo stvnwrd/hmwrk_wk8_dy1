d7d3cb67908ef3cd8a04a6d5f5dcdd3c4eb8dd54
Steven Ward <34653526+stvnwrd@users.noreply.github.com>
2018-03-12 22:18:05 +0000

    Most CRUD added - Needs review

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,33 @@
+import db.DBHelper;
 import models.Author;
+import models.Book;
+
+import java.util.List;
 
 public class Runner {
 
     public static void main(String[] args) {
 
-        Author author1 = new
+        Author author1 = new Author("Iain Banks");
+        DBHelper.save(author1);
+
+        Author author2 = new Author("Muriel Spark");
+        DBHelper.save(author2);
+
+        Book book1 = new Book("The Wasp Factory", author1);
+        DBHelper.save(book1);
+
+        Book book2 = new Book("The Prime of Miss Jean Brodie", author2);
+        DBHelper.save(book2);
+
+        book1.setTitle("Complicity");
+        DBHelper.update(book1);
+
+        DBHelper.deleteById(book2.getId(),"Book");
+
+        List<Book> books = DBHelper.getAll("Book");
+
+        List<Author> authors = DBHelper.getAll("Author");
+
     }
 }
--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -28,6 +28,23 @@ public class DBHelper {
         }
     }
 
+    public static void deleteById(int id, String className){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try{
+            transaction = session.beginTransaction();
+            String hql = "delete from " + className + " where id = :id";
+            Query query = session.createQuery(hql);
+            query.setInteger("id", id);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
     public static void delete(Object object) {
         session = HibernateUtil.getSessionFactory().openSession();
         try {
@@ -57,23 +74,42 @@ public class DBHelper {
         }
     }
 
-    public static List<Book> getBooks(int id) {
+    public static <T> List<T> getAll(String className){
         session = HibernateUtil.getSessionFactory().openSession();
-        List<Book> books = null;
+        List<T> results = null;
         try {
             transaction = session.beginTransaction();
-            String hql = "from Book WHERE author_id = :id";
-            Query query = session.createQuery(hql);
-            query.setInteger("id", id);
-            books = query.list();
-            transaction.commit();
-
-        } catch (HibernateException e){
+            String hql = "from " + className;
+            results = session.createQuery (hql)
+.list();
+        transaction.commit();
+        } catch (HibernateException e) {
             transaction.rollback();
             e.printStackTrace();
         } finally {
             session.close();
         }
-        return books;
+        return results;
     }
+
+
+//    public static List<Book> getBooks(int id) {
+//        session = HibernateUtil.getSessionFactory().openSession();
+//        List<Book> books = null;
+//        try {
+//            transaction = session.beginTransaction();
+//            String hql = "from Book WHERE author_id = :id";
+//            Query query = session.createQuery(hql);
+//            query.setInteger("id", id);
+//            books = query.list();
+//            transaction.commit();
+//
+//        } catch (HibernateException e){
+//            transaction.rollback();
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//        return books;
+//    }
 }
